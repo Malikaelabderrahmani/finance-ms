32880891cb185590037163dfc199337a738b1d37
Malikaelabderrahmani <malikarahmani67@gmail.com>
2025-04-16 15:51:54 +0100

    create finance project

---
+++ financems/src/main/java/pfe/mandomati/financems/FinancemsApplication.java
@@ -0,0 +1,13 @@
+package pfe.mandomati.financems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancemsApplication.class, args);
+	}
+
+}
---
+++ financems/src/test/java/pfe/mandomati/financems/FinancemsApplicationTests.java
@@ -0,0 +1,13 @@
+package pfe.mandomati.financems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
