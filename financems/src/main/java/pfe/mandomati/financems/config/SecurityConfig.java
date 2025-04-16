aa703c1f68c5e3f1fc3eec7b6be2638128c602b3
Malikaelabderrahmani <malikarahmani67@gmail.com>
2025-04-16 16:15:34 +0100

    test the app

---
+++ financems/src/main/java/pfe/mandomati/financems/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package pfe.mandomati.financems.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // Désactive la protection CSRF (utile pour les APIs)
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll() // Permet toutes les requêtes sans authentification
+            );
+
+        return http.build();
+    }
+}
